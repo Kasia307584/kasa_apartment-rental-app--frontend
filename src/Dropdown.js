e42c23d62ffaa1030ec6684795b357ab650b33c7
Kasia307584 <63410273+Kasia307584@users.noreply.github.com>
2023-05-22 11:51:35 +0200

    Dropdown onClick activated

--- src/Dropdown.js
+++ src/Dropdown.js
@@ -1,14 +1,35 @@
+import { useState } from "react";
+
 export default function Dropdown(props) {
   const { className, name, text } = props;
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  function openDropdown() {
+    setIsOpen(true);
+  }
+  function closeDropdown() {
+    setIsOpen(false);
+  }
 
   return (
     <article className={`dropmenu ${className}`}>
       <button className="dropbtn">
         {name}
-        <i className="fas fa-chevron-down"></i>
-        <i className="fas fa-chevron-up"></i>
+        <i
+          className="fas fa-chevron-down"
+          onClick={openDropdown}
+          style={{ display: isOpen ? "none" : "block" }}
+        ></i>
+        <i
+          className="fas fa-chevron-up"
+          onClick={closeDropdown}
+          style={{ display: isOpen ? "block" : "none" }}
+        ></i>
       </button>
-      <ul className="text">{text}</ul>
+      <ul className="text" style={{ display: isOpen ? "block" : "none" }}>
+        {text}
+      </ul>
     </article>
   );
 }
